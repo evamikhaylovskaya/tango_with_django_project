73794e231c7bb743ef379da37973a9f3545a4c23
2833792m@student.gla.ac.uk <2833792m@student.gla.ac.uk>
2024-02-09 14:44:06 +0000

    Chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,help_text="Please enter the category name.")
@@ -39,3 +40,17 @@ class PageForm(forms.ModelForm):
 class Meta:
     model = Page
     exclude = ('category',)
+    
+    
+    
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+        
+        
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)    
